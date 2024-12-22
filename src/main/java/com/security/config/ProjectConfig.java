603c248fb8628f198c4df530bd471ea13d65608d
tajbirahmed <tajbir26@gmail.com>
2024-12-22 12:55:15 +0600

    added default way of configuring UserDetailsService, PasswordEncoder, SecurityFilterChain

---
+++ src/main/java/com/security/config/ProjectConfig.java
@@ -0,0 +1,44 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        UserDetails user = User
+                .withUsername("tajbir")
+                .password("123456")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+        return http.build();
+    }
+}
