ddc9ac379d68522b3ff61801caa4f1fabc4dbf9e
tajbirahmed <tajbir26@gmail.com>
2024-12-22 20:09:59 +0600

    added DataSource(mysql) for keeping users in the Database

--- src/main/java/com/security/config/ProjectConfig.java
+++ src/main/java/com/security/config/ProjectConfig.java
@@ -1,19 +1,29 @@
 package com.security.config;
 
-import com.security.components.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 public class ProjectConfig {
 
-    private final CustomAuthenticationProvider authenticationProvider;
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
